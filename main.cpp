889cbf70822c8e6bc7f8f9035d61e34f7f81e212
Ogata <masato20051119@gmail.com>
2023-03-28 02:56:38 +0900

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "memory.h"
+
+#include <thread>
+
+namespace offset
+{
+	constexpr ::std::ptrdiff_t dwLocalPlayer = 0xDEB964;
+
+	constexpr ::std::ptrdiff_t dwClientState = 0x59F19C;
+	constexpr ::std::ptrdiff_t dwClientState_ViewAngles = 0x4D90;
+
+	constexpr ::std::ptrdiff_t m_aimPunchAngle = 0x303C;
+	constexpr ::std::ptrdiff_t m_iShotsFired = 0x103E0;
+}
+
+int main()
+{
+	while (true)
+	{
+		std::this_thread
+	}
+	return 0;
+}
